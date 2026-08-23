4c91bac79888030a0721b455efd9e201f6ad47d8
mwagner3122 <wagner.matthewjj@gmail.com>
2026-08-23 11:01:36 -0500

    Phase 4: feature_compute thin re-export

---
+++ src/feature_compute.py
@@ -0,0 +1,4 @@
+"""Thin re-export for phase-4 feature build."""
+
+from src.feature_asof import load_frames, prior_weight, shrink  # noqa: F401
+from src.feature_build import compute_feature_frame  # noqa: F401
